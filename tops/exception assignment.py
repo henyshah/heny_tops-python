f0c593a6a2520e4ac9408dcf66b16985750337a2
henyshah <henyshah@itrvn.com>
2023-03-27 11:44:20 +0530

    updated exception assignment

--- tops/exception assignment.py
+++ tops/exception assignment.py
@@ -28,35 +28,38 @@
 # ANS.  it simply evaluates to false and does not raise any exception.
 
 # How Do You Handle Exceptions With Try/Except/Finally In Python? Explain with coding snippets
-# ANS. try:
-        #    exception code
-    #    except:
-            # statement
-        # else:
-            # without exception
-        # finally:
-            # it always occurs
+# ANS. 
+"""
+try:
+        exception code
+    except:
+            statement
+    else:
+            without exception
+    finally:
+            it always occurs
+    """
 # example:
-# try:
-# # variable definition
-#     a=10
-#     b=20
-#     ans=a+b
-#     print(ans)
-# except:
-#     print("invalid input")
-# else:
-#     print("Welcome to math world")
-#     # it always execute exception occurs or not
-# finally:
-#     print("THANK YOU FOR USING THIS APPLICATION")
+try:
+# variable definition
+    a=10
+    b=20
+    ans=a+b
+    print(ans)
+except:
+    print("invalid input")
+else:
+    print("Welcome to math world")
+    # it always execute exception occurs or not
+finally:
+    print("THANK YOU FOR USING THIS APPLICATION")
 
 # Write python program that user to enter only odd numbers, else will raise an exception. 
-# while True:
-#     try:
-#         num = int(input("enter an odd number: "))
-#         if num % 2 == 0:
-#             raise ValueError("Even numbers are not allowed.")
-#         break
-#     except ValueError as e:
-#         print(e)
+while True:
+    try:
+        num = int(input("enter an odd number: "))
+        if num % 2 == 0:
+            raise ValueError("Even numbers are not allowed.")
+        break
+    except ValueError as e:
+        print(e)
