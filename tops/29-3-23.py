0732dc87bb041d602deeb920f759150d75e0767d
henyshah <henyshah@itrvn.com>
2023-03-29 09:55:36 +0530

    29-3-23

--- tops/29-3-23.py
+++ tops/29-3-23.py
@@ -107,7 +107,7 @@ def viewstudent:
 # try:
 #     # check condition
 #     if n1>n2:
-#         print("n1 is greater than n2")
+#         print("n2 is greater than n1")
 #     else:
 #         # raise exception
 #         raise smallerException
