6426b915f22910861fee12073b76e7d195e7d14b
henyshah <henyshah@itrvn.com>
2023-04-11 21:46:41 +0530

    module 4

--- tops/Module – 4 (Advance python programming).py
+++ tops/Module – 4 (Advance python programming).py
@@ -29,7 +29,7 @@ f.close()
 
 # Write a Python program to read first n lines of a file.
 # accept line from the user
-n = int(input("\n\t\tEnter Lines To Read : "))
+n = int(input("Enter Lines: "))
 # open any file in specific variable 
 f = open("myfile.txt","r")
 # use for loop
@@ -48,28 +48,80 @@ for i in range(n):
 	# read line
 	print(f.readline())
 
-# Write a Python program to read last n lines of a file. 
-
 
 # Write a Python program to read a file line by line and store it into a list
+# txt file
+filename="myfile.txt"
+# create empty list
+line=[]
+with open(filename, "r") as file:
+    for line in file:
+        line.append(line.strip())
+        # print line
+print(line)
 
 
 # Write a Python program to read a file line by line store it into a variable. 
+# txt file
+filename="myfile.txt"
+# create empty string
+lines=""
+with open(filename, "r") as file:
+    for line in file:
+        lines += line
+        # print lines
+print(lines)
 
 
 # Write a python program to find the longest words. 
+# define function
+def find_longest_words(text):
+    words = text.split()
+    # creating empty list
+    longest_words = []
+    max_length = max(len(word) for word in words)
+    for word in words:
+        if len(word) == max_length:
+            longest_words.append(word)
+    return longest_words
+text = "hellooo,my name is heny"
+longest_words = find_longest_words(text)
+print("Longest words: ", longest_words)
 
 
 # Write a Python program to count the number of lines in a text file. 
+# txt file
+filename = "myfile.txt"
+num_lines = 0
+with open(filename, "r") as file:
+    for line in file:
+        num_lines += 1
+# print no. of lines
+print("no. of lines:", num_lines)
 
 
 # Write a Python program to count the frequency of words in a file. 
 
 
-# Write a Python program to write a list to a file. 
-
+# Write a Python program to write a list to a file.
+# creating the list
+my_list = ["apple", "banana", "orange", "mango"]
+filename = "my_list.txt"
+with open(filename, "w") as file:
+    for item in my_list:
+        file.write(item + "\n")
+# print list to the file
+print("done successfully!!")
+ 
 
 # Write a Python program to copy the contents of a file to another file.
+# taking two files
+file1 = "myfile.txt"
+file2 = "mynewfile.txt"
+with open(file1, "r") as file1, open(file2, "w") as file2:
+    file2.write(file1.read())
+# content copied from one file to the another file
+print("done successfully!!")
 
 
 # Explain Exception handling? What is an Error in Python?
@@ -214,15 +266,65 @@ obj.display()
 
 
 # Write a Python class named Rectangle constructed by a length and width and a method which will compute the area of a rectangle 
+# take class named rectangle
+class Rectangle:
+    # define init,length and width
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        # define area 
+    def area(self):
+        return self.length * self.width
+    # taking length=5,width=10
+rect = Rectangle(5, 10)
+# print the area of rectangle
+print(rect.area())  
 
 
 # Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle 
-
-
-# Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle 
+# import math library
+import math
+# create class circle
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        # define area
+    def area(self):
+        return math.pi * (self.radius ** 2)
+    # define perimeter
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+circle = Circle(5)
+# print area
+print(circle.area()) 
+# print perimeter   
+print(circle.perimeter())  
 
 
 # Explain Inheritance in Python with an example? What is init? Or What Is A Constructor In Python? 
+# inheritance: one class derives properties of another class
+#  it provides reusabilities 
+#  by using of inheritance we can reduce our code
+#  types:
+#  1)single level inheritance
+#  2)multi level inheritance
+#  3)multiple inheritance
+#  4)heierarchical inheritance
+#  5)hybrid inheritance
+# example:
+class parent:
+    def home(self):
+        print("Ahmedabad")
+class Child(parent):    #ineritance
+    def car(self):
+        print("I HAVE CAR")
+        # obj declaration
+obj = Child()
+obj.home()
+obj.car()
+# init:
+# __init__.py : it indicates any folder as a package.
+# constructor is a unique function that gets called automatically when an object is created of a class.
 
 
 # What is Instantiation in terms of OOP terminology?
@@ -230,9 +332,61 @@ obj.display()
 
 
 # What is used to check whether an object o is an instance of class A? 
+# ANS. isinstance() Function  
 
 
 # What relationship is appropriate for Course and Faculty? 
+class faculty:
+    # setter method 
+    def setId(self,id):
+        self.id=id
+    # getter method 
+    def getId(self):
+        return self.id
+    def setName(self,name):
+        self.name=name
+    def getName(self):
+        return self.name
+    def setTechnology(self,technology):
+        self.technology=technology
+    def getTechnology(self):
+        return self.technology
+obj=faculty()
+# set data
+id=int(input("enter id : "))
+obj.setId(id)
+# get id using getter method 
+print(obj.getId())
+# set name - using setter
+name=input("enter name: ")
+obj.setName(name)
+# get data
+print(obj.getName())
+course=input("enter course: ")
+obj.setTechnology(course)
+print(obj.getcourse())
 
 
 # What relationship is appropriate for Student and Person?
+class student:
+    # setter method 
+    def setId(self,id):
+        self.id=id
+    # getter method 
+    def getId(self):
+        return self.id
+    def setName(self,name):
+        self.name=name
+    def getName(self):
+        return self.name
+obj=student()
+# set data
+id=int(input("enter id : "))
+obj.setId(id)
+# get id using getter method 
+print(obj.getId())
+# set name - using setter
+name=input("enter name: ")
+obj.setName(name)
+# get data
+print(obj.getName())
