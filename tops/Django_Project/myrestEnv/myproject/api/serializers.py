f246002fe39106541a63332bdab0c084f1dfe870
henyshah <henyshah@itrvn.com>
2023-06-23 09:30:33 +0530

    api

---
+++ tops/Django_Project/myrestEnv/myproject/api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+class StudentSerializer(serializers.Serializer):
+    name=serializers.CharField(max_length=100)
+    email=serializers.EmailField(max_length=100)
+    city=serializers.CharField(max_length=30)
+
--- tops/Django_Project/myrestEnv/myproject/api/views.py
+++ tops/Django_Project/myrestEnv/myproject/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import *
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 # Create your views here.
+
+def student(request):
+    sall=Student.objects.all()
+    serializer=StudentSerializer(sall,many=True)
+    # serializer data convert into json format
+    json_data=JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,content_type="application/json")
+
+
+def student_get(request):
+    sall=Student.objects.get(id=2)
+    serializer=StudentSerializer(sall)
+    # serializer data convert into json format
+    json_data=JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,content_type="application/json")
